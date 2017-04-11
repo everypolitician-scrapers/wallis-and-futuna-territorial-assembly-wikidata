070ea8055b064f31a972521fb6b557a60b2b0dd9
Tony Bowden <tony@tmtm.com>
2017-04-11 09:08:03 +0100

    Scrape Wikipedia directly
    The Wikipedia scraper no longer tries to extract a Wikiname column, so
    we need to scrape that page again directly ourselves.

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,12 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/wallis-and-futuna-territorial-assembly', column: 'wikiname')
+names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: "https://fr.wikipedia.org/wiki/Assembl%C3%A9e_territoriale_des_%C3%AEles_Wallis_et_Futuna",
+  after: '//span[@id="Composition"]',
+  before: '//span[@id="Ancien_.C3.A9lu_de_2012_.C3.A0_2013"]',
+  xpath: './/li//a[not(@class="new")]/@title',
+)
+
 EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names })
+
