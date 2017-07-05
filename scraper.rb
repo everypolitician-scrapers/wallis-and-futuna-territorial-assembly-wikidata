d28bf99b367deac1e48e70fe9ca1d2a13c55a0ef
Tony Bowden <tony@tmtm.com>
2017-07-05 10:03:04 +0100

    Change heading to fetch up to
    Page layout has changed

--- scraper.rb
+++ scraper.rb
@@ -6,7 +6,7 @@ require 'wikidata/fetcher'
 names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: "https://fr.wikipedia.org/wiki/Assembl%C3%A9e_territoriale_des_%C3%AEles_Wallis_et_Futuna",
   after: '//span[@id="Composition"]',
-  before: '//span[@id="Ancien_.C3.A9lu_de_2012_.C3.A0_2013"]',
+  before: '//span[@id="Notes_et_r.C3.A9f.C3.A9rences"]',
   xpath: './/li//a[not(@class="new")]/@title',
 )
 
