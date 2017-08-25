3fc9fd09fffa4bbc4176b251451dd334c49a9a40
Tony Bowden <tony@tmtm.com>
2017-08-25 10:11:59 +0100

    Also fetch anyone pre-matched in EveryPolitician
    If we already care about someone, don't lose them just becaus they're no
    longer on the Wikipedia page.

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,16 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
+existing = EveryPolitician::Index.new.country('Wallis-and-Futuna').lower_house.popolo.persons.map(&:wikidata).compact
+
 names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: "https://fr.wikipedia.org/wiki/Assembl%C3%A9e_territoriale_des_%C3%AEles_Wallis_et_Futuna",
+  url: 'https://fr.wikipedia.org/wiki/Assembl%C3%A9e_territoriale_des_%C3%AEles_Wallis_et_Futuna',
   after: '//span[@id="Composition"]',
   before: '//span[@id="Notes_et_r.C3.A9f.C3.A9rences"]',
-  xpath: './/li//a[not(@class="new")]/@title',
+  xpath: './/li//a[not(@class="new")]/@title'
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names })
-
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { fr: names })
